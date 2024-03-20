d1eefd6adac1e3d5a1f5575e83f29ec5e348f0fe
Sabrine Barreto <sabrine0barreto@gmail.com>
2024-03-20 17:12:03 -0300

    Project created

---
+++ src/test/java/com/training/Course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.Course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
