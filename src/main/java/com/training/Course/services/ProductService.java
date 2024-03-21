b06192ac696a915b87191432cd2f373b08ae13c4
Sabrine Barreto <sabrine0barreto@gmail.com>
2024-03-21 17:38:03 -0300

    Many-to-many association with JoinTable

---
+++ src/main/java/com/training/Course/services/ProductService.java
@@ -0,0 +1,28 @@
+package com.training.Course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.training.Course.entities.Product;
+import com.training.Course.respositories.ProductRepository;
+
+@Service 
+public class ProductService {
+
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll(){
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional <Product> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+
+}
