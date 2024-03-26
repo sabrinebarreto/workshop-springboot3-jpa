c333daa274f7d67ef61c85fd66f6c959d821af75
Sabrine Barreto <sabrine0barreto@gmail.com>
2024-03-26 18:16:58 -0300

    Payment, one-to-one association

--- src/main/java/com/training/Course/config/TesteConfig.java
+++ src/main/java/com/training/Course/config/TesteConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.training.Course.entities.Category;
 import com.training.Course.entities.Order;
 import com.training.Course.entities.OrderItem;
+import com.training.Course.entities.Payment;
 import com.training.Course.entities.Product;
 import com.training.Course.entities.User;
 import com.training.Course.entities.enums.OrderStatus;
@@ -81,7 +82,12 @@ public class TesteConfig implements CommandLineRunner{
 
 	
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
-
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 	
 	
